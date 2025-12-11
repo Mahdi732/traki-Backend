117dcc74c6c11136e00cefe93221e9b57768c75b
Mahdi732 <ey449751@gmail.com>
2025-12-11 17:17:29 +0100

    TRAKI-33 Configure MongoDB connection with Mongoose

---
+++ src/database/connection.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+class DBConnection {
+  #mongoUri;
+
+  constructor () {
+    this.#mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/fleet';
+  }
+
+  async connect () {
+    try {
+      console.log('database is trying to connect. please wait a second...');
+      await mongoose.connect(this.#mongoUri);
+      console.log('database is connected with success!');
+    } catch (e) {
+      console.error('database error :', e.message);
+      throw e;
+    }
+  }
+
+  async disconnect() {
+    await mongoose.disconnect();
+    console.log('MongoDB disconnected');
+  }
+}
+
+export default new DBConnection();
