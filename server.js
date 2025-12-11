73d8f07b875210f54e02856255d24811f7b4b6e1
Mahdi732 <ey449751@gmail.com>
2025-12-11 17:15:22 +0100

    TRAKI-32 Initialize Express.js server with basic configuration

---
+++ server.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import App from "./src/app.js";
+
+async function startServer() {
+    try {
+        const server = new App();
+        await server.serverStart();
+        
+        console.log("__________________________________");
+        console.log("Server is ready to receive requests");
+        console.log("__________________________________");
+    } catch (error) {
+        console.error("Failed to start server:", error);
+        process.exit(1);
+    }
+}
+
+startServer();
